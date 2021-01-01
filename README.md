4e9bea2b58e0cb6ecce57eef18d2f1ba600319cc
Albin Groen <albin.groen@gmail.com>
2021-01-01 18:32:42 +0100

    :sparkles: Dark mode, banner, and updated docs

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 > Render any markdown on a white page through a simple url
 
-⚠️ REMEMBER TO URL ENCODE YOUR MARKDOWN ⚠️
-
 ```javascript
-const markdown = '...'
+const markdown = '# Title'
 
-const url = `https://[DOMAIN_COMING_SOON]?markdown=${encodeURIComponent(markdown)}`
+const url = `https://rmmd.link/?markdown=${encodeURIComponent(markdown)}`
 
 <a href={markdown}>Share markdown</a>
 ```
+
+## Query parameters
+
+| Name     | Type    | Required | Default   | Description                                                          |
+|----------|---------|----------|-----------|----------------------------------------------------------------------|
+| markdown | String  | true     | undefined | A raw markdown string that will be rendered as HTML on the page.     |
+| darkMode | Boolean | false    | false     | Setting this to true will render a dark page instead of a light one. |
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Editor from "rich-markdown-editor";
 
 export default function Home() {
   const router = useRouter();
-  const { markdown } = router.query;
+  const { markdown, darkMode } = router.query;
+
+  useEffect(() => {
+    if (darkMode === "true") {
+      document.body.style.background = "#181a1b";
+      document.body.style.color = "#fff";
+    }
+  }, [darkMode]);
 
   return (
     <div>
@@ -15,10 +23,22 @@ export default function Home() {
 
       <Editor
         defaultValue={markdown as string}
+        dark={darkMode === "true"}
         key={markdown as string}
         onChange={() => {}}
         readOnly
       />
+
+      <p className="banner">
+        Shared using{" "}
+        <a
+          href="https://github.com/albingroen/render-my-markdown"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          render-my-markdown
+        </a>
+      </p>
     </div>
   );
 }
